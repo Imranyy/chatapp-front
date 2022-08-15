7093c50a952f9036a4686802f5ca73c3095b1fb0
imranyy <imranmatano6@gmail.com>
2022-08-15 14:10:38 +0300

    update on name and time

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,6 +8,7 @@ const socket=io.connect('https://serve-chat-app.herokuapp.com')
     const[output,setOutput]=useState('');
     const [output1,setOutput1]=useState('')
     const [message,setMessage]=useState('');
+    
     const publicVapidKey='BJ3OlUIn9QFTDtL9rUamLHYpUvMR8NgxOCWcpOSeJv7OenqN-zdt-i3sMAuRkMWsCwtE5sCDPnnccIkXZiCIf5Q';
 
    
@@ -16,59 +17,59 @@ const socket=io.connect('https://serve-chat-app.herokuapp.com')
         const form=document.querySelector('.d-flex')
         try {
                 socket.emit('chat',{
+                time:current_time,    
                 pic:localStorage.getItem('pic'),
                 name:localStorage.getItem('name'),
                 message:message
             })
-
             //check for sw
-            if('serviceWorker' in navigator){
-                send().catch(err=>console.error(err));
-            }
-            //register sw, register push, send push
-            async function send(){
-                //registering sw
-                console.log('Registering sw...')
-                const register=await navigator.serviceWorker.register('/sw.js',{
-                    scope:'/'
-                });
-                console.log('sw registered...');
-                //registering push
-                console.log('registering push...');
-                const subscription=await register.pushManager.subscribe({
-                    userVisibleOnly:true,
-                    applicationServerKey:urlBase64ToUint8Arry(publicVapidKey)
-                });
-                console.log('push registered...');
-                //send push notification
-                console.log('sending push...')
-                await fetch('https://serve-chat-app.herokuapp.com/subscribe',{
-                    method:'POST',
-                    body:JSON.stringify({
-                        subscription,
-                        name:localStorage.getItem('name'),
-                        pic:localStorage.getItem('pic'),
-                        message:message
-                    }),
-                    headers:{
-                        'content-type':'application/json'
-                    }
-                });
-                console.log('push sent...')
-            }
-
-            function urlBase64ToUint8Arry(base64String){
-                const padding= '='.repeat((4-base64String.length % 4) %4);
-                const base64=(base64String + padding)
-                .replace(/\-/g, '+')
-                .replace(/_/g,'/');
-                const rawData=window.atob(base64);
-                const outputArray=new Uint8Array(rawData.length);
-                for(let i=0;i<rawData.length;++i){
-                    outputArray[i]=rawData.charCodeAt(i)
+                if('serviceWorker' in navigator){
+                    send().catch(err=>console.error(err));
                 }
-                return outputArray;
-            }
+                //register sw, register push, send push
+                async function send(){
+                    //registering sw
+                    console.log('Registering sw...')
+                    const register=await navigator.serviceWorker.register('/sw.js',{
+                        scope:'/'
+                    });
+                    console.log('sw registered...');
+                    //registering push
+                    console.log('registering push...');
+                    const subscription=await register.pushManager.subscribe({
+                        userVisibleOnly:true,
+                        applicationServerKey:urlBase64ToUint8Arry(publicVapidKey)
+                    });
+                    console.log('push registered...');
+                    //send push notification
+                    console.log('sending push...')
+                    await fetch('https://serve-chat-app.herokuapp.com/subscribe',{
+                        method:'POST',
+                        body:JSON.stringify({
+                            subscription,
+                            name:localStorage.getItem('name'),
+                            pic:localStorage.getItem('pic'),
+                            message:message
+                        }),
+                        headers:{
+                            'content-type':'application/json'
+                        }
+                    });
+                    console.log('push sent...');
+                }
+
+                function urlBase64ToUint8Arry(base64String){
+                    const padding= '='.repeat((4-base64String.length % 4) %4);
+                    const base64=(base64String + padding)
+                    .replace(/\-/g, '+')
+                    .replace(/_/g,'/');
+                    const rawData=window.atob(base64);
+                    const outputArray=new Uint8Array(rawData.length);
+                    for(let i=0;i<rawData.length;++i){
+                        outputArray[i]=rawData.charCodeAt(i)
+                    }
+                    return outputArray;
+                }
 
             form.reset()
         } catch (error) {
@@ -79,29 +80,47 @@ const socket=io.connect('https://serve-chat-app.herokuapp.com')
         useEffect(()=>{
             //get recent chats
             socket.on('chat',data=>{
-                console.log(data)
                 setOutput1(data)
                 })
             //getting chats
         socket.on('output',res=>{
             setOutput(res)
-            console.log(res)
             
        },[socket])
         })
+
+        //month,date,time
+        let today=new Date();
+          //time
+         let hour=addZero(today.getHours());
+         let min=addZero(today.getMinutes());
+
+         function addZero(num){
+            return num <10?`0${num}`:num;
+         }
+
+         let current_time=`${hour}:${min}`;
     return(
         <>
         <NavBar/>
         <div className="container chat-window">
         {output?output.map(res=>(
             <div key={res._id} className="output">
-                <img src={res.pic?res.pic:img} className="avatar"  alt='.' width='40' height='40' style={{borderRadius:'20px'}}/><p style={{marginLeft:'50px',marginTop:'-45px'}}>  {res.message}</p><br/>
+                <img src={res.pic?res.pic:img} className="avatar"  alt='.' width='40' height='40' style={{borderRadius:'20px'}}/><div style={{marginLeft:'52px',marginTop:'-45px'}}>  {res.message} 
+                <br/><h6 style={{marginTop:'10px',fontSize:'12px',fontFamily:'monospace', color:'GrayText'}}>From: {res.name}</h6>
+                <p style={{float:'right',marginTop:'-1px',fontSize:'12px',fontFamily:'monospace', color:'GrayText'}}>{res.time}</p>
+                </div>
+                <br/>
             </div>
                 )):'No chats'}
    
                 
                 <div className="output1">
-                    <img src={output1.pic?output1.pic:img} className="avatar"  alt='.' width='40' height='40' style={{borderRadius:'20px'}}/><p style={{marginLeft:'50px',marginTop:'-45px'}}>  {output1.message?output1.message:'Text something😉😉'}</p><br/>
+                    <img src={output1.pic?output1.pic:img} className="avatar"  alt='.' width='40' height='40' style={{borderRadius:'20px'}}/><div style={{marginLeft:'50px',marginTop:'-45px'}}>  {output1.message?output1.message:'Text something😉😉'}
+                    <br/><h6 style={{marginTop:'10px',fontSize:'12px',fontFamily:'monospace', color:'GrayText'}}>From: {output1.name?output1.name:'Crappygam😂'}</h6>
+                    <p style={{float:'right',marginTop:'-1px',fontSize:'12px',fontFamily:'monospace', color:'GrayText'}}>{output1.time}</p>
+                    </div>
+                    <br/>
                  </div>
                  
 
